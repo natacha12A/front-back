8a0b648de4883f92b4a37d42822b319fbcd18c95
Natacha Tavares <natacha.alves@eaportal.org>
2025-11-06 09:29:40 -0300

    segundo commit hehe

--- pages/cadastro.js
+++ pages/cadastro.js
@@ -4,11 +4,12 @@ button.onclick = (event) => {
     signUpEagle()
 }
 
-function signUpEagle() {
+async function signUpEagle() {
     const name = document.querySelector("#name").value
     const email = document.querySelector("#email").value
     const age = document.querySelector("#age").value
     const nickname = document.querySelector("#nickname").value
+    const password = document.querySelector("#password").value
 
     if(name === "" || email === "" || age === "" || nickname === ""){
         alert("preencha todas as informações abaixo!")
@@ -19,8 +20,21 @@ function signUpEagle() {
         name,
         email,
         age,
-        nickname
+        nickname,
+        password
     }
 
     console.log(user)
+
+    //enviar para o backend
+    const response = await fetch("http://localhost:3333/cadastrar", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ user })
+    }).then(response => response.json())
+
+    const {message} = response
+    alert(message)
 }
